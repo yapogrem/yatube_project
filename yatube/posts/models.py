02db99d6047c9a3cbeb304ea12b50d4ef7f897fa
pogrem <ya0961@yandex.ru>
2022-11-22 07:08:04 -0500

    Добавлены изменения models.py admin.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -12,4 +20,14 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+
+
+        
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,31 +1,54 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 
 # Главная страница
+# def index(request):
+#     template = 'posts/index.html'
+#     title = 'Это главная страница проекта Yatube'
+#     context = {
+#         'title': title,
+#         }
+#     return render(request, template, context) 
+
 def index(request):
-    template = 'posts/index.html'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
     title = 'Это главная страница проекта Yatube'
+    # В словаре context отправляем информацию в шаблон
     context = {
         'title': title,
-        }
-    return render(request, template, context) 
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-
-# def index(request):    
-#     return HttpResponse('Главная страница')
-
-
-# Страница со списком мороженого
-def group_posts(request):
-    return HttpResponse('Список мороженого')
 
 # Главная страница
-def group_posts(request):
-    template = 'posts/group_list.html'
+# def group_posts(request):
+#     template = 'posts/group_list.html'
+#     title = 'Здесь будет информация о группах проекта Yatube'
+#     context = {
+#         'title': title,
+#         }
+#     return render(request, template, context)
+
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
     title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'title': title,
-        }
-    return render(request, template, context)
-
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
